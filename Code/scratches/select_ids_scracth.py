31e0a93a29009aa79485ebb2dae6c0c70649ce41
Pierrick Royer <pierrick.royer@inrae.fr>
2024-12-18 16:55:37 +0100

    load and save project; with observations

---
+++ Code/scratches/select_ids_scracth.py
@@ -0,0 +1,6 @@
+
+        ####LOAD OBS
+# for file in os.listdir("C:/Users/royerpie/Documents/rootDoc/automate/observations/PseudoAGMIP"):
+#     excel_file_path = os.path.join("C:/Users/royerpie/Documents/rootDoc/automate/observations/PseudoAGMIP", file)
+#
+#     HandleDB.load_obs_db(Constant.db_config, excel_file_path, Constant.daily_dict, Constant.summary_dict, Constant.mapping_dict)
